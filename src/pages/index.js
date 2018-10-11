a4a6800f067a1e76ecf827bfb68443b589071497
Dana Asbury <danaasbury@gmail.com>
2018-10-11 00:33:00 -0500

    feature: add cart service and logic to save session value

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,55 @@ import { Link } from 'gatsby'
 import { ProductPreview } from '../components/ProductPreview';
 import Layout from '../components/layout'
 import { GridList } from 'react-md';
-
 import './index.scss'
+
+const { getCartBy } = require('../services/cart-service');
+const uuidv4 = require('uuid/v4');
+
 
 class IndexPage extends Component {
   constructor(props) {
     super(props);
     this.data = props.data;
-    this.state = {
-      count: 0,
-      cart: new Map() //eventually will be a call to get data from backend
-    }
+    this.state = { count: 0, cart: new Map(), sessionId: null }
+
+
   }
 
+  componentDidMount() {
+    let sessionId;
+    if (sessionStorage) {
+      sessionId = sessionStorage.getItem('sesionId');
+    }
+    const isNewSession = (sessionStorage && sessionId);
+
+    if (isNewSession) {
+      sessionId = new uuidv4();
+      sessionStorage.setItem('sessionId', sessionId);
+    }
+
+    const cartPromise = isNewSession ? new Promise(resolve => resolve({ count: 0, cart: new Map(), sessionId })) : getCartBy(sessionId);
+    cartPromise.then(cart => {
+      console.log(cart);
+      this.setState(cart);
+    });
+  }
+
+
+  objToStrMap = (obj) => {
+    let strMap = new Map();
+    for (let k of Object.keys(obj)) {
+      strMap.set(k, obj[k]);
+    }
+    return strMap;
+  }
+
+  jsonToStrMap = (jsonStr) => {
+    return this.objToStrMap(JSON.parse(jsonStr));
+  }
+
+
   addToCart = (product, index) => {
-    console.log('add to cart', product);
     this.setState(({ cart, count }) => {
       const key = `${product.id}_${index}`;
       const prev = cart.get(key);
@@ -28,10 +62,9 @@ class IndexPage extends Component {
     })
   }
 
-
   offsetItemCount = (key, offset) => {
     this.setState(({ cart, count }) => {
-      
+
       const item = cart.get(key);
       const newCount = item.count + offset;
       const newState = (newCount <= 0) ?
@@ -67,8 +100,9 @@ class IndexPage extends Component {
   }
 
   render() {
+
     return (
-      <Layout cart={this.state.cart} cartManager={{increment: this.incrementItemCount, decrement: this.decrementItemCount, remove: this.removeFromCart}}>
+      <Layout cart={this.state.cart} cartManager={{ increment: this.incrementItemCount, decrement: this.decrementItemCount, remove: this.removeFromCart }}>
         {
           <GridList size={3} container="pictures" component="section" className='examples-page'>
             {
